029ede6e72631164201ac67241ce54e55c2b7fd7
Andrey <k-udm@ya.ru>
2023-09-08 09:42:03 +0200

    Lesson 12

---
+++ src/main/java/lesson12/task1/Degree.java
@@ -0,0 +1,17 @@
+package lesson12.task1;
+
+public enum Degree {
+
+    BACHELOR("Бакалавр"),
+    MASTER("Магистр");
+
+    private String description;
+
+    Degree(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ src/main/java/lesson12/task1/Student.java
@@ -0,0 +1,10 @@
+package lesson12.task1;
+
+public class Student {
+
+    private String name;
+    private int age;
+    private int course;
+    private double averageRate;
+
+}
